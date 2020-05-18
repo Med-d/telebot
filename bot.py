c0a67e110183aa7ef15489b3005add8bbe8ef999
Med-d <meddd.bomj@gmail.com>
2020-05-18 10:05:56 +0500

    update 1

--- bot.py
+++ bot.py
@@ -4,9 +4,13 @@ import os
 import shutil
 import pymysql.cursors
 
+#Constants
 SUPER = 'super user'
 SIMPLE = 'simple user'
 
+#Connection to MySQL database
+#Server have db "active_users" with 2 tables
+#Now uses for initialize users
 connect = pymysql.connect(host = 'localhost',
                              user = 'root',
                              password = config.passwordSQL,
@@ -14,6 +18,8 @@ connect = pymysql.connect(host = 'localhost',
                              charset = 'utf8',
                           cursorclass = pymysql.cursors.DictCursor)
 
+#Find user in db
+#If user not found add in table with simple users
 def find_user(chat_id, username):
     with connect.cursor() as cursor:
         cursor.execute("select chat_id from super_users")
@@ -27,9 +33,11 @@ def find_user(chat_id, username):
         cursor.execute("insert simple_users(chat_id, name) values ("+str(chat_id)+", '"+str(username)+"');")
         return SIMPLE
 
+#Connecting to bot
 bot = telebot.TeleBot(config.TOKEN)
 path = "photos"
 
+#Download all files from path to user
 def WalkOnFiles(path, user):
     directory = os.listdir(path)
     for file in directory:
@@ -40,25 +48,27 @@ def WalkOnFiles(path, user):
         if not ('.' in file):
             WalkOnFiles(path+'\\'+file, user)
 
-@bot.message_handler(commands = ["pullout"])
-def pull_out_All_Files(message):
-    user_type = find_user(message.chat.id, message.chat.username)
-    #file = open('photos\\1.jpg', 'rb')
-    #bot.send_photo(message.chat.id, file)
-    WalkOnFiles(path, message.chat.id)
-    shutil.rmtree(path)
-    os.mkdir(path)
-    bot.send_message(message.chat.id, "Pulled out successfully")
+#commands for bot
+#@bot.message_handler(commands = ["pullout"])
+#def pull_out_All_Files(message):
+#    user_type = find_user(message.chat.id, message.chat.username)
+#    #file = open('photos\\1.jpg', 'rb')
+#    #bot.send_photo(message.chat.id, file)
+#    WalkOnFiles(path, message.chat.id)
+#    os.mkdir(path)
+#    bot.send_message(message.chat.id, "Pulled out successfully")
 
 @bot.message_handler(commands = ["show_id"])
 def show_ID(message):
     user_type = find_user(message.chat.id, message.chat.username)
     bot.send_message(message.chat.id, message.chat)
 
+#echo
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     user_type = find_user(message.chat.id, message.chat.username)
     bot.send_message(message.chat.id, message.text)
 
+#Bot don't stop
 if __name__ == '__main__':
      bot.polling(none_stop=True)
